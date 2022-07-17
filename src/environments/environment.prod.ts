057ff39823da015e034ff9143494f305dc7a6464
Loon-7w7 <99504287+Loon-7w7@users.noreply.github.com>
2022-07-17 13:19:11 -0500

    Implementación de edición de un jugador de la tabla

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBq1YUgag5A2jodKKVa4h-mysTNOuo0Zig',
+    authDomain: 'typescript-con-angular-377e7.firebaseapp.com',
+    databaseURL:'https://typescript-con-angular-377e7-default-rtdb.firebaseio.com',
+    projectId: 'typescript-con-angular-377e7',
+    storageBucket: 'typescript-con-angular-377e7.appspot.com',
+    messagingSenderId: '808315761451',
+    appId: '1:808315761451:web:277ed623fa7c51c2104b6e',
+    measurementId: 'G-5SNZE8DC6L',
+}
+}
