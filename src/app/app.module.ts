9e9c6e8c8665ab7cf4377eb3026f42d93d84a896
Loon-7w7 <99504287+Loon-7w7@users.noreply.github.com>
2022-07-17 09:49:42 -0500

    Iiniciando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule} from '@angular/fire/compat'
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,6 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    //AngularFireModule.initializeApp(environment.firebaseConfig),
+    //AngularFireDatabaseModule,
     AppRoutingModule
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBq1YUgag5A2jodKKVa4h-mysTNOuo0Zig',
+    authDomain: 'typescript-con-angular-377e7.firebaseapp.com',
+    databaseURL:'https://typescript-con-angular-377e7-default-rtdb.firebaseio.com',
+    projectId: 'typescript-con-angular-377e7',
+    storageBucket: 'typescript-con-angular-377e7.appspot.com',
+    messagingSenderId: '808315761451',
+    appId: '1:808315761451:web:277ed623fa7c51c2104b6e',
+    measurementId: 'G-5SNZE8DC6L',
+  },
 };
 
 /*
